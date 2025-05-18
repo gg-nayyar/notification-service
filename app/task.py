d2c49d4b1605f812313d311b17539811e20f5fcd
gg-nayyar <krishsenpai7@gmail.com>
2025-05-18 12:47:29 +0530

    update: rabbitMQ is now getting used withCloudAMQP instead of locally

--- app/task.py
+++ app/task.py
@@ -1,5 +1,5 @@
 import os
-from celery import Celery
+from celery import Celery, Task
 from dotenv import load_dotenv
 from app.services.sms_service import send_sms
 from app.services.email_service import send_email
@@ -8,12 +8,18 @@ load_dotenv()
 
 celery = Celery("tasks", broker=os.getenv("CELERY_BROKER_URL"))
 
-@celery.task
+class RetryTask(Task):
+    autoretry_for = (Exception,)
+    retry_kwargs = {"max_retries": 5, "countdown": 7}
+    retry_backoff = True
+    retry_jitter = True
+
+@celery.task(base=RetryTask)
 def send_email_task(email:str, message:str):
     print("[DEBUG] Sending email task")
     send_email(email, message)
     
-@celery.task
+@celery.task(base=RetryTask)
 def send_sms_task(phone:str, message:str):
     print("[DEBUG] Sending SMS task")
     send_sms(phone, message)
